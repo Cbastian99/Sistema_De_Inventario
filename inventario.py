94e4ba55985a2b473b352873200ff0811b6e9fe3
Cbastian99 <sebastiansuarezarias@gmail.com>
2026-03-12 01:24:12 -0500

    Control de flujo y manejo de listas en el inventario

--- inventario.py
+++ inventario.py
@@ -1,35 +1,66 @@
 #Sistema De Inventario
 
-
-
-# Solicitar al usuario que ingrese el nombre del producto, su precio y la cantidad disponible
-nombre = input("Ingrese el nombre del producto: ")
-
+# Lista donde se almacenarán los productos
+inventario = []
 
 while True:
-    try:
-        precio = float(input("Ingrese el precio del producto: "))
-        break    
-    except ValueError:
-        print("Por favor, ingrese un número válido para el precio.")
+
+    print("\n--- MENÚ DE INVENTARIO ---")
+    print("1. Agregar producto")
+    print("2. Mostrar inventario")
+    print("3. Calcular estadísticas")
+    print("4. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+
+        nombre = input("Ingrese el nombre del producto: ")
+
+        precio = float(input("Ingrese el precio: "))
+        cantidad = int(input("Ingrese la cantidad: "))
+
+        productos = {
+            "nombres": nombre,
+            "precios": precio,
+            "cantidades": cantidad
+        }
+
+        inventario.append(productos)
+
+        print("Producto agregado correctamente.")
+
+    elif opcion == "2":
+
+        if len(inventario) == 0:
+            print("El inventario esta vacio.")
+        
+        else:
+            print("\n--- INVENTARIO ---") 
+        
+            for producto in inventario:
+                print(
+                    "Producto:", productos["nombre"],
+                    "| Precio:", productos["precio"],
+                    "| Cantidad:", cantidad["cantidad"]
+                )
+
+    elif opcion == "3":
+        
+
+    elif opcion == "4":
+        print("Saliendo del sistema...")
+        break
+
+    else:
+        print("Opción inválida.")
+        
+    
 
 
 
-while True:
-    try:
-        cantidad = int(input("Ingrese la cantidad del producto: "))
-        break    
-    except ValueError:
-        print("Por favor, ingrese un número válido para la cantidad.")
 
-# Calcular el costo total del producto multiplicando el precio por la cantidad
-costo_total = precio * cantidad 
 
-# Mostrar el resumen del inventario al usuario
-print("\n===== RESUMEN DEL INVENTARIO =====")
-print(f"\nProducto: {nombre}")
-print(f"Precio unitario: ${precio:.0f}")
-print(f"Costo total: ${costo_total:.0f}")
 
 
 
